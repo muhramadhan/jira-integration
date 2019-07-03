e7fd5a3c91fb17cc3918ac5fddb4f65b0d3d8218
muhramadhan <ramadhanm1998@gmail.com>
2019-07-03 14:10:15 +0700

    tried to do transition

--- app.go
+++ app.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"net/http"
 
+	"github.com/andygrunwald/go-jira"
 	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
-func main() {
-	// tp := jira.BasicAuthTransport{
-	// 	Username: "ramadhanm1998@gmail.com",
-	// 	Password: "icB26nXqVx90BRVTrxKKB68F",
-	// }
+var regexProjectKey = "\\[[A-Z]*\\-[0-9]+\\]"
+var port = ":8080"
 
-	// client, _ := jira.NewClient(tp.Client(), "https://m-f-hafizh.atlassian.net/")
+func main() {
+	tp := jira.BasicAuthTransport{
+		Username: "ramadhanm1998@gmail.com",
+		Password: "icB26nXqVx90BRVTrxKKB68F",
+	}
+
+	client, _ := jira.NewClient(tp.Client(), "https://m-f-hafizh.atlassian.net/")
 
 	hook, _ := github.New(github.Options.Secret("1234567890"))
 	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +28,7 @@ func main() {
 		if err != nil {
 			if err == github.ErrEventNotFound {
 				fmt.Println("No event found")
+				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 		}
@@ -34,8 +41,27 @@ func main() {
 
 		case github.PullRequestPayload:
 			pullRequest := payload.(github.PullRequestPayload)
+			reg, _ := regexp.Compile(regexProjectKey)
+			title := pullRequest.PullRequest.Title
+			issueKey := strings.Replace(strings.Replace(reg.FindString(title), "[", "", -1), "]", "", -1)
+			issue, _, err := client.Issue.Get(issueKey, nil)
+			transitions, _, err := client.Issue.GetTransitions(issueKey)
+			if err != nil {
+				fmt.Println("Error : ", err)
+			}
+			fmt.Println(issue.Fields.Summary)
+			fmt.Println(transitions[1].ID)
 			// Do whatever you want from here...
-			fmt.Printf("%+v", pullRequest)
+			if pullRequest.Action == "edited" {
+				res, err := client.Issue.DoTransition(issue.ID, transitions[1].ID)
+				if err != nil {
+					fmt.Println("Error : ", err)
+				}
+				fmt.Println("response : ", res)
+			}
+			// fmt.Printf("%+v", pullRequest)
 		}
 	})
+	fmt.Printf("Running on port %s\n", port)
+	http.ListenAndServe(port, nil)
 }
